3dfd34eb22a923afdffea3c2c9aa1cbfb7f93ca5
Marcus Gustafsson <marcusgu@hotmail.com>
2017-12-25 18:48:42 +0100

    removed index.test

--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,14 @@
 const request = require('supertest');
 const app = require('./index');
 
-describe('Test the root path', () => {
-    test('It should response the GET method', (done) => {
-        request(app).get('/').then((response) => {
-            expect(response.statusCode).toBe(200);
-            done();
-        });
-    });
-});
+// describe('Test the root path', () => {
+//     test('It should response the GET method', (done) => {
+//         request(app).get('/').then((response) => {
+//             expect(response.statusCode).toBe(200);
+//             done();
+//         });
+//     });
+// });
 
 describe('Test non correct path', () => {
     test('It should response a 404 with the GET method', (done) => {
